34725104f7b80d72adba978df522c7a79cf0b3ff
Cassandra <cassandra.sw@live.fr>
2022-11-09 12:06:28 +0100

    ça marche =)

--- pages/register.php
+++ pages/register.php
@@ -1,18 +1,21 @@
-<!-- <?php
-$dbh = new PDO("mysql:dbname=btc-vintage-car-auction;host=127.0.0.1;port=8889", "root", "root");
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $dbh = new PDO("mysql:dbname=cars_btc;host=localhost", "root", "");
 
-$query = $dbh->prepare("
-INSERT TO CAR (lastname, firstname, email, password, confirm) VALUES ($_POST[lastname], $_POST[firstname], $_POST[email], $_POST[password], $_POST[confirm]
-")
+  $query = $dbh->prepare(
+    "INSERT INTO users (`lastname`,`firstname`,`email`,`password`) 
+    VALUES (?, ?, ?, ?)"
+  );
 
+  $result = $query->execute([$_POST['lastname'], $_POST['firstname'], $_POST['email'], $_POST['password']]);
 
-
+  $query->debugDumpParams();
+}
 ?>
 
 
-<!DOCTYPE html>
 
-<html>
+
 
 <head>
 
@@ -21,13 +24,12 @@ INSERT TO CAR (lastname, firstname, email, password, confirm) VALUES ($_POST[las
 <body>
   <section>
 
-    <form action="" method="$_POST">
-      <input type="text" name="lastname" placeholder="nom" value=<?php echo $_POST['lastname'] ?> required />
-      <input type="text" name="firstname" placeholder="prenom" value=<?php echo $_POST['firstname'] ?> required />
-      <input type="email" name="email" placeholder="email" value=<?php echo $_POST['email'] ?> required />
-      <input type="password" name="password" placeholder="mot de passe" value=<?php echo $_POST['password'] ?> required />
-      <input type="password" name="confirm" placeholder="confirmer votre mot de passe" value=<?php echo $_POST['confirm'] ?> required />
-      <button>Valider</button>
+    <form action="" method="POST">
+      <input type="text" name="lastname" placeholder="nom" value="<?php echo $_POST['lastname'] ?? "" ?>" required />
+      <input type="text" name="firstname" placeholder="prenom" value="<?php echo $_POST['firstname'] ?? "" ?>" required />
+      <input type="email" name="email" placeholder="email" value="<?php echo $_POST['email'] ?? "" ?>" required />
+      <input type="password" name="password" placeholder="mot de passe" value="<?php echo $_POST['password'] ?? "" ?>" required />
+      <button type="submit">Valider</button>
     </form>
   </section>
 
@@ -35,5 +37,3 @@ INSERT TO CAR (lastname, firstname, email, password, confirm) VALUES ($_POST[las
 
 
 </body>
-
-</html> -->
