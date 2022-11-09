7a2ee6926c6a8d64b4f595322b2f7292e5b7487b
Thom <thomayor@gmail.com>
2022-11-09 11:31:23 +0100

    "input relié  à la db sur create auction"

--- pages/create_auction.php
+++ pages/create_auction.php
@@ -2,28 +2,29 @@
 
 <?php
 
-$dbh= new PDO("mysql:dbname=btc-vintage-car-auction;host=127.0.0.1;port=8889","root","root");
+$dbh= new PDO("mysql:dbname=CAR;host=127.0.0.1;port=8889","root","root");
 
-$query= $dbh->prepare("
-INSERT INTO cars (model,brand,power,year,description,price,img)
-VALUES ($_POST[model],$_POST[brand],$_POST[power],$_POST[year],$_POST[description],$_POST[price],$_POST[img])
-")
+$query = $dbh->prepare("INSERT INTO cars (`model`,`brand`,`power`,`year`,`description`,`price`,`img`) VALUES (?,?,?,?,?,?,?)");
+            
+$result = $query->execute([$_POST['model'], $_POST['brand'], $_POST['power'],$_POST['year'],$_POST['description'],$_POST['price'],$_POST['img']]);
 
+$query->debugDumpParams();
+var_dump($params);
 ?>
 
 
 <!-- formulaire -->
 <h1> Déposer une annonce </h1>
 
-<form action="" method="$_POST">
+<form action="" method="POST">
 
-    <input type="text" name="brand"placeholder="Marque" value="<?php echo $_POST["brand"] ?>">
-    <input type="text" name="model" placeholder="Modèle de voiture" value="<?php echo $_POST["model"] ?>">
-    <input type="text" name="power" placeholder="Puissance (cv)" value="<?php echo $_POST["power"] ?>">
-    <input type="text" name="year"placeholder="Années" value="<?php echo $_POST["year"] ?>">
-    <textarea name="description" value="<?php echo $_POST["description"] ?>"> Commentez les informations de votre annonce</textarea>
-    <input type="text" name="price" placeholder="prix de vente" value="<?php echo $_POST["price"] ?>">
-    <input type="file" name="img" value="<?php echo $_POST["img"] ?>">
+    <input type="text" name="brand" placeholder="Marque" value="<?php echo $_POST[`brand`] ?>">
+    <input type="text" name="model" placeholder="Modèle de voiture" value="<?php echo $_POST[`model`] ?>">
+    <input type="text" name="power" placeholder="Puissance (cv)" value="<?php echo $_POST[`power`] ?>">
+    <input type="text" name="year"placeholder="Années" value="<?php echo $_POST[`year`] ?>">
+    <textarea name="description" value="<?php echo $_POST[`description`] ?>" placeholder="Commentez votre bolide"></textarea>
+    <input type="text" name="price" placeholder="prix de vente" value="<?php echo $_POST[`price`] ?>">
+    <input type="file" name="img" value="<?php echo $_POST[`img`] ?>">
     <button type="submit"> Envoyer</button>
 
 </form>
