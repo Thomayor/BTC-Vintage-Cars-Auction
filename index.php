86b2d5dc3c0f4364ac03dfb69a702c4a39bc3afa
Thom <thomayor@gmail.com>
2022-11-09 12:17:59 +0100

    affichage auctions

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
-$dbh= new PDO("mysql:dbname=btc-vintage-car-auction;host=127.0.0.1;port=8889","root","root");
+$dbh= new PDO("mysql:dbname=CAR;host=127.0.0.1;port=8889","root","root");
 
+$query= $dbh->prepare("SELECT model,brand,power,year,description,price,img from cars");
+$query->execute();
+$result=$query->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+foreach ($result as $car) {
+    echo '<br>';
+    echo '<br>';
+    echo 'Vintage Car: ' . $car[""];
+    echo '<ul>';
+    foreach ($car as $key => $value) {
+      echo "<li>" . $key . ' : ' . $value . "</li>";
+    }
+    echo "</ul>";
+    echo '<br>';
+  }
 
 
 ?>  
