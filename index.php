b7786fa7651195272d98217bc93592f353dcf895
Cassandra <cassandra.sw@live.fr>
2022-11-13 16:25:43 +0100

    ok

--- index.php
+++ index.php
@@ -4,20 +4,36 @@
 ?>
 
 
-<!DOCTYPE html>
 
-<html>
+
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+    <link href="index.css" rel="stylesheet" />
 </head>
 
 <body>
-
-
-
-
-
+    <div class="background">
+        <nav class="navbar">
+            <img class="navbar-logo" src="" />
+            <nav class="menu">
+                <a href="#" style="
+    padding-right: 15px">Home</a>
+                <a href="#" style="
+    padding-right: 15px">New Auction</a>
+                <a href="#" style="
+    padding-right: 15px">Connexion</a>
+                <a href="#" style="
+    padding-right: 15px">Inscription</a>
+            </nav>
+            <input type="text" placeholder="Search" class="navbar-search" />
+        </nav>
+    </div>
 </body>
 
 </html>
